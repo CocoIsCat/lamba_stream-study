85ea313085a1d01ad3dc2dcdbd7e2c1b6d6d60b0
cocoiscat <demy176@naver.com>
2023-03-08 21:23:07 +0900

    stream 클래스 #6

--- src/main/java/StreamEx6.java
+++ src/main/java/StreamEx6.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class StreamEx6 {
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class StreamEx6 {
+    public static void main(String[] args) {
+        Stream<String[]> strArrStrm = Stream.of(
+                new String[] {"abc", "def", "jkl"},
+                new String[] {"ABC", "DEF", "JKL"}
+        );
+
+        Stream<String> stringStream = strArrStrm.flatMap(Arrays::stream);
+        stringStream.map(String::toLowerCase)
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
+        System.out.println();
+
+        String[] lineArr = {
+                "Believe or not It is true",
+                "Do or do not There is no try"
+        };
+
+        Arrays.stream(lineArr)
+                .flatMap(line -> Stream.of(line.split(" ")))
+                .map(String::toLowerCase)
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
+        System.out.println();
+
+    }
 }
